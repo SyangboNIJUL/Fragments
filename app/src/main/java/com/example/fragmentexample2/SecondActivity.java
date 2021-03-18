6910e5472ee8f08a7f9d6d32c7adbf518a977438
Nijul syangbo <nijullama96@gmail.com>
2021-03-18 10:43:58 +0545

    Homework

--- app/src/main/java/com/example/fragmentexample2/SecondActivity.java
+++ app/src/main/java/com/example/fragmentexample2/SecondActivity.java
@@ -1,53 +1,45 @@
 package com.example.fragmentexample2;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
+
     private Button mButton;
     private Boolean isFragmentDisplayed = false;
     static final String STATE_FRAGMENT = "state_of_fragment";
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         mButton = findViewById(R.id.open_button);
+        Intent intent = getIntent();
 
         if (savedInstanceState != null) {
             isFragmentDisplayed =
                     savedInstanceState.getBoolean(STATE_FRAGMENT);
             if (isFragmentDisplayed) {
 
-                mButton.setText(R.string.close);
+                mButton.setText(R.string.prev);
             }
         }
-
-
-
-
     }
-
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
-
 
         savedInstanceState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
         super.onSaveInstanceState(savedInstanceState);
 
 
     }
+
 
     public void displayFragment() {
         SimpleFragment simpleFragment = SimpleFragment.newInstance();
@@ -56,8 +48,57 @@ public class MainActivity extends AppCompatActivity {
                 .beginTransaction();
         fragmentTransaction.add(R.id.fragment_container,
                 simpleFragment).addToBackStack(null).commit();
-        mButton.setText(R.string.close);
+        mButton.setText(R.string.prev);
         isFragmentDisplayed = true;
+
+    }
+
+    public void prevFragment() {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager
+                .findFragmentById(R.id.fragment_container);
+        if (simpleFragment != null) {
+
+            FragmentTransaction fragmentTransaction =
+                    fragmentManager.beginTransaction();
+            fragmentTransaction.remove(simpleFragment).commit();
+        }
+
+        mButton.setText(R.string.next);
+
+        isFragmentDisplayed = false;
+    }
+
+    public void launchSecondActivity(View view) {
+
+        if (!isFragmentDisplayed) {
+            displayFragment();
+        } else {
+           prevFragment();
+        }
+    }
+
+    public void returnReply(View view)
+    {
+        Intent replyIntent = new Intent();
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+
+    public void launchMainActivity(View view) {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onClick(View view) {
+        if (!isFragmentDisplayed) {
+            displayFragment();
+        } else {
+            closeFragment();
+        }
 
     }
 
@@ -77,18 +118,5 @@ public class MainActivity extends AppCompatActivity {
         mButton.setText(R.string.open);
 
         isFragmentDisplayed = false;
-    }
-
-    public void onClick(View view) {
-        if (!isFragmentDisplayed) {
-            displayFragment();
-        } else {
-            closeFragment();
-        }
-    }
-
-    public void launchSecondActivity(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
     }
 }
